19b92ff6f8040105db05990ab091d2ebbd4f2b37
Johnathan Leppert <johnathan@kespry.com>
2015-09-28 22:48:06 -0700

    basic deployment/versioning

--- gulpfile.js
+++ gulpfile.js
@@ -18,8 +18,12 @@ var watchify   = require('watchify'),
     sass       = require('gulp-sass'),
     livereload = require('gulp-livereload'),
     testem     = require('testem'),
-    s3         = require('knox'),
-    Promise    = require('bluebird');
+    knox       = require('knox'),
+    aws        = require('aws-sdk'),
+    Promise    = require('bluebird'),
+    git        = require('git-rev');
+
+var args = require('minimist')(process.argv.slice(2));
 
 function initBundler(dev) {
   if(!global.b && dev) {
@@ -96,13 +100,6 @@ function S3PutPromisifier(method) {
   }
 }
 
-Promise.promisifyAll(s3, {
-  promisifier: S3PutPromisifier,
-  filter: function(name) {
-    if(name === 'put') return true;
-  }
-});
-
 function initStyleWatcher() {
   if(!global.styleWatcher) {
     global.styleWatcher = chokidar.watch('./src/browser/**/*.scss').on('change', function() {
@@ -114,25 +111,29 @@ function initStyleWatcher() {
 function renderIndex(manifest, versioned, done) {
   var index    = dust.compileFn(fs.readFileSync('./src/browser/index.dust', 'utf8'), 'index');
  
-  var cleaned = {};
-  Object.keys(manifest).forEach(function(key) {
-    cleaned[key.replace('.', '-')] = manifest[key];
-  });
+  git.short(function(hash) {
+    var cleaned = {};
+    Object.keys(manifest).forEach(function(key) {
+      cleaned[key.replace('.', '-')] = '/' + hash + '/' + manifest[key];
+    });
 
-  index(cleaned, function(err, out) {
-    if(err) {
-      gutil.log(err);
-      done();
-    } else {
-      var filename;
-      if(versioned) {
-        filename = manifest['bundle.js'].replace('bundle-', '').replace('.js', '') + manifest['app.css'].replace('app-', '').replace('.css', '') + '.html';
+    // TODO: move files
+
+    index(cleaned, function(err, out) {
+      if(err) {
+        gutil.log(err);
+        done();
       } else {
-        filename = 'index.html';
+        var filename;
+        if(versioned) {
+          filename = manifest['bundle.js'].replace('bundle-', '').replace('.js', '') + manifest['app.css'].replace('app-', '').replace('.css', '') + '.html';
+        } else {
+          filename = 'index.html';
+        }
+        fs.writeFileSync('./var/build/' + filename, out);
+        done(filename);
       }
-      fs.writeFileSync('./var/build/' + filename, out);
-      done(filename);
-    }
+    });
   });
 }
 
@@ -151,10 +152,10 @@ gulp.task('publish', ['bundle', 'styles'], function(done) {
     renderIndex(manifest, true, function(filename) {
       var env = JSON.parse(fs.readFileSync('./.env.json'));
 
-      var client = s3.createClient({
+      var client = knox.createClient({
         key: env.aws.accessKey,
         secret: env.aws.secretKey,
-        region: 'us-west-1',
+        region: env.aws.region,
         bucket: env.aws.publishBucket
       });
 
@@ -167,34 +168,44 @@ gulp.task('publish', ['bundle', 'styles'], function(done) {
 
       var uploads = [];
       var indexSrc = fs.readFileSync('./var/build/' + filename);
-      var indexReq = client.putAsync(filename, {
-        'Content-Length': Buffer.byteLength(indexSrc),
-        'Content-Type': 'text/html',
-      }, indexSrc);
-      uploads.push(indexReq);
+      git.short(function(hash) {
+        var indexReq = client.putAsync('/' + hash + '/' + filename, {
+          'Content-Length': Buffer.byteLength(indexSrc),
+          'Content-Type': 'text/html',
+        }, indexSrc);
+        uploads.push(indexReq);
 
-      Object.keys(manifest).forEach(function(file) {
-        var src = fs.readFileSync('./var/build/' + manifest[file]);
-        var req = client.putAsync(manifest[file], {
-          'Content-Length': Buffer.byteLength(src)
-        }, src);
+        Object.keys(manifest).forEach(function(file) {
+          var src = fs.readFileSync('./var/build/' + manifest[file]);
+          var req = client.putAsync('/' + hash + '/' + manifest[file], {
+            'Content-Length': Buffer.byteLength(src)
+          }, src);
 
-        uploads.push(req);
-      });
-
-      Promise.all(uploads).then(function success(items) {
-        items.forEach(function(item) {
-          gutil.log("Successfully uploaded file", item[1]);
+          uploads.push(req);
         });
-        done();
-      }, function failure(items) {
-        items.forEach(function(item) {
-          gutil.log("Error uploading file", item[1], item[0], item[2]);
+
+        manifest.index = filename;
+        var manifestStr = JSON.stringify(manifest);
+        var manifestReq = client.putAsync('/' + hash + '/index.json', {
+          'Content-Length': Buffer.byteLength(manifestStr),
+          'Content-Type': 'application/json'
+        }, manifestStr);
+        uploads.push(manifestReq);
+
+        Promise.all(uploads).then(function success(items) {
+          items.forEach(function(item) {
+            gutil.log("Successfully uploaded file", item[1]);
+          });
+          done();
+        }, function failure(items) {
+          items.forEach(function(item) {
+            gutil.log("Error uploading file", item[1], item[0], item[2]);
+          });
+          done();
+        }).catch(function(e) {
+          gutil.log("Exception occured uploading one or more files", e);
+          process.exit(1);
         });
-        done();
-      }).catch(function(e) {
-        gutil.log("Exception occured uploading one or more files", e);
-        process.exit(1);
       });
     });
   });
@@ -246,6 +257,89 @@ gulp.task('rev', function(done) {
       merge: true
     }))
     .pipe(gulp.dest('./var/build'));
+});
+
+gulp.task('list', function(done) {
+  var env = JSON.parse(fs.readFileSync('./.env.json'));
+
+  var client = knox.createClient({
+    key: env.aws.accessKey,
+    secret: env.aws.secretKey,
+    region: env.aws.region,
+    bucket: env.aws.publishBucket
+  });
+
+  client.list({}, function(err, data) {
+    if(err) {
+      gutil.log("Error listing contents of bucket", env.aws.publishBucket);
+    } else {
+      data.Contents.forEach(function(item) {
+        gutil.log(item.Key, item.Size);
+      });
+    }
+
+    done();
+  });
+});
+
+gulp.task('set-active', function(done) {
+  var env = JSON.parse(fs.readFileSync('./.env.json'));
+
+  aws.config.update({
+    accessKeyId: env.aws.accessKey, 
+    secretAccessKey: env.aws.secretKey,
+    region: env.aws.region 
+  });
+
+  var s3 = new aws.S3({ params: { Bucket: env.aws.publishBucket }});
+ 
+  s3.getObject({
+    Bucket: env.aws.publishBucket,
+    Key: args.sha + '/index.json'
+  }, function(err, data) {
+    if(err) {
+      gutil.log("Error getting index config from S3", err);
+    } else {
+      var config = JSON.parse(data.Body.toString());
+      console.log(config);
+      s3.copyObject({
+        Bucket: env.aws.publishBucket,
+        CopySource: args.sha + '/' + config.index,
+        ACL: 'public-read',
+        Key: 'index.html'
+      }, function(err, data) {
+        if(err) {
+          console.log(this);
+          gutil.log("Error occured copying object from S3", err);
+        } else {
+          gutil.log("Set current index to", args.sha + '/' + config.index);
+          done();
+        }
+      });
+    }
+  });
+});
+
+gulp.task('show-active', function(done) {
+  var env = JSON.parse(fs.readFileSync('./.env.json'));
+
+  aws.config.update({
+    accessKeyId: env.aws.accessKey, 
+    secretAccessKey: env.aws.secretKey,
+    region: env.aws.region 
+  });
+
+  var s3 = new aws.S3({ params: { Bucket: env.aws.publishBucket }});
+  
+  s3.getBucketWebsite({
+    Bucket: env.aws.publishBucket
+  }, function(err, data) {
+    if(err) {
+      gutil.log("Error occured getting bucket details from S3", err);
+    } else {
+      gutil.log("Active Index Document:", data.IndexDocument);
+    }
+  });
 });
 
 gulp.task('bundle', bundle());
